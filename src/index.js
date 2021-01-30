6b25bed128c2f55f134bb4dcea824787fc650abf
vishalbalakrishnan <vishal.bala07@gmail.com>
2021-01-30 09:53:36 +0530

    made component as react property

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class App extends React.Component {
+class App extends Component {
   render() {
-    return ( <
-      div className = "App" >
-      <
-      h1 > Hello, React! < /h1> < /
-      div >
+    return (
+      <div className="App" >
+        <h1 > Hello, React! </h1>
+      </div >
     )
   }
 }
-ReactDOM.render( < App / > , document.getElementById('root'))
+ReactDOM.render(< App />, document.getElementById('root'))
